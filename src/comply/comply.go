631a76770429747f676372c038e7a1d10475766c
CalvinRodo <calvinv@gmail.com>
2019-07-05 15:37:35 -0400

    Adding check for 2fa

--- src/comply/comply.go
+++ src/comply/comply.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/shurcooL/githubv4"
@@ -10,8 +11,9 @@ import (
 )
 
 func main() {
+
 	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+		&oauth2.Token{AccessToken: os.Getenv("COMPLIANCE_CHECKER")},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 
@@ -22,14 +24,22 @@ func main() {
 			Login     githubv4.String
 			CreatedAt githubv4.DateTime
 		}
+		Organization struct {
+			Name                            githubv4.String
+			RequiresTwoFactorAuthentication githubv4.Boolean
+		} `graphql:"organization(login: \"esdc-edsc\")"`
 	}
+
 	// Use client...
 	err := client.Query(context.Background(), &query, nil)
 
 	if err != nil {
+		log.Fatal(err)
 		// Handle error.
 	}
 	fmt.Println("    Login:", query.Viewer.Login)
 	fmt.Println("CreatedAt:", query.Viewer.CreatedAt)
+	fmt.Println("Org Name: ", query.Organization.Name)
+	fmt.Println("2fa Required: ", query.Organization.RequiresTwoFactorAuthentication)
 
 }
